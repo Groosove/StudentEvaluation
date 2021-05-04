a406321bee2e3ee85f626f0af748958f54992ee8
Groosove <arturianec2013@gmail.com>
2021-05-04 15:20:23 +0300

    Start write application

---
+++ StudentEvaluation/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  StudentEvaluation
+//
+//  Created by Fenix Lavon on 5/4/21.
+//  Copyright © 2021 Fenix Lavon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+
+
+}
+
