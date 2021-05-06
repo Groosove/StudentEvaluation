0c1e4a437ea24030d37243563987641fb38b5276
Groosove <arturianec2013@gmail.com>
2021-05-06 15:58:45 +0300

    Add charecter validation

--- StudentEval/StudentEditViewController.swift
+++ StudentEval/StudentEditViewController.swift
@@ -29,6 +29,7 @@ class StudentEditViewController: UIViewController {
 	@IBOutlet weak var averageEvalLabel: UITextField!
 	
 	@IBAction func saveButton(_ sender: Any) {
+		if !checkLabel() || !newStudent { return }
 		let managedContext = CoreDataStack.managedObjectContext
 		let entity = NSEntityDescription.entity(forEntityName: "Student", in: managedContext)!
 		let student = (indexPath != nil) ? delegate?.student[indexPath!.row] : NSManagedObject(entity: entity, insertInto: managedContext)
@@ -57,15 +58,14 @@ class StudentEditViewController: UIViewController {
 		alertSecondeName.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in NSLog("The \"OK\" alert occured.")}))
 		let alertAverageEval = UIAlertController(title: "Error", message: "Оценка может быть только от 1...5", preferredStyle: .alert)
 		alertAverageEval.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in NSLog("The \"OK\" alert occured.")}))
-		
-		guard firstNameLabel.text?.rangeOfCharacter(from: CharacterSet.controlCharacters) == nil &&
-			firstNameLabel.text?.rangeOfCharacter(from: CharacterSet.whitespacesAndNewlines) == nil &&
-			firstNameLabel.text?.rangeOfCharacter(from: CharacterSet.decimalDigits) == nil &&
-			firstNameLabel.text?.rangeOfCharacter(from: CharacterSet.letters) != nil else { self.present(alertFirstName, animated: true, completion: nil); return false }
-		guard secondNameLabel.text?.rangeOfCharacter(from: CharacterSet.controlCharacters) == nil &&
-			secondNameLabel.text?.rangeOfCharacter(from: CharacterSet.whitespacesAndNewlines) == nil &&
-			secondNameLabel.text?.rangeOfCharacter(from: CharacterSet.decimalDigits) == nil &&
-			secondNameLabel.text?.rangeOfCharacter(from: CharacterSet.letters) != nil else { self.present(alertFirstName, animated: true, completion: nil); return false }
+		guard firstNameLabel.text?.rangeOfCharacter(from: CharacterSet.decimalDigits) == nil &&
+			firstNameLabel.text?.rangeOfCharacter(from: CharacterSet.punctuationCharacters) == nil &&
+			firstNameLabel.text?.rangeOfCharacter(from: CharacterSet.whitespacesAndNewlines) == nil
+			else { self.present(alertFirstName, animated: true, completion: nil); return false }
+		guard firstNameLabel.text?.rangeOfCharacter(from: CharacterSet.decimalDigits) == nil &&
+			firstNameLabel.text?.rangeOfCharacter(from: CharacterSet.punctuationCharacters) == nil &&
+			firstNameLabel.text?.rangeOfCharacter(from: CharacterSet.whitespacesAndNewlines) == nil
+			else { self.present(alertFirstName, animated: true, completion: nil); return false }
 		
 		guard let value = UInt8(averageEvalLabel.text!) else { self.present(alertAverageEval, animated: true, completion: nil); return false }
 		
